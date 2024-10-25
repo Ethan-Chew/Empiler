4407229507e319eb4e027c1ea38d5f80f164cafc
Ethan Chew <ethanchewmh@gmail.com>
2024-10-25 17:47:51 +0800

    feat: login auth

--- back-end/models/user.js
+++ back-end/models/user.js
@@ -1,3 +1,5 @@
+const supabase = require('../db/supabase');
+
 export default class User {
     constructor(id, username, password, email, role) {
         this.id = id;
@@ -5,5 +7,19 @@ export default class User {
         this.password = password;
         this.email = email;
         this.role = role;
+    }
+
+    static async getUserWithUsername(username) {
+        const queryRequest = await supabase
+            .from('users')
+            .select('*')
+            .eq('username', username);
+
+        if (queryRequest.status !== 200) {
+            console.log(queryRequest.error);
+            return null;
+        }
+
+        return queryRequest.data;
     }
 }
