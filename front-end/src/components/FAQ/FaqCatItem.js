cf3e046a2e24ae158ec733d820c91b4c8d227b85
Hervin Sie <hervsie@gmail.com>
2024-10-31 18:11:47 +0800

    feat: added hover effects to FAQCat

--- front-end/src/components/FAQ/FaqCatItem.js
+++ front-end/src/components/FAQ/FaqCatItem.js
@@ -1,6 +1,6 @@
 export default function FaqCatItem({ icon, title, description, link }) {
     return (
-        <div className="flex bg-white shadow rounded-lg w-full">
+        <div className="flex bg-white shadow rounded-lg w-full shadow-md hover:shadow-lg transition-shadow duration-200">
             <div className="w-2 bg-purple-500 rounded-l-lg"></div>
             <div className="flex items-center w-full p-4">
                 <h2 className="font-semibold text-lg pr-4 text-black">{title}</h2>
