e024586374dbccd8f4fddcf2a0d186859feccd9f
Ethan-Chew <ethanchewmh@gmail.com>
2024-11-10 13:34:55 +0800

    feat: display recommend booking after some time

--- front-end/src/pages/Customer/InitialiseChat.js
+++ front-end/src/pages/Customer/InitialiseChat.js
@@ -2,7 +2,10 @@ import { useState, useEffect } from 'react';
 import { socket } from '../../utils/chatSocket';
 import { useNavigate } from 'react-router-dom';
 import * as CryptoJS from 'crypto-js';
+
+// Components
 import Navbar from '../../components/Navbar';
+import { BsCalendarCheck } from "react-icons/bs";
 
 export default function InitialiseChat() {
     const navigate = useNavigate();
@@ -10,9 +13,9 @@ export default function InitialiseChat() {
     const [waitingTime, setWaitingTime] = useState(0);
     const [connectionErr, setConnectionErr] = useState(false);
 
-    // Search Params Data
-    const [faqSection, setFaqSection] = useState('');
-    const [faqQuestion, setFaqQuestion] = useState('');
+    // Reccomend make Appointment
+    const [custWaitingTime, setCustWaitingTime] = useState(0);
+    const [displayReccoAppt, setDisplayReccoAppt] = useState(false);
 
     // Handle Connect and Disconnect Events
     const handleConnection = () => {
@@ -48,18 +51,15 @@ export default function InitialiseChat() {
         socket.emit('customer:leave');
     };
 
-    const handleDisconnectionButton = () => {
-        setIsConnected(false);
-        sessionStorage.removeItem('customerSessionIdentifier');
-        socket.emit('customer:leave');
-        navigate('/');
-    };
+    // const handleDisconnectionButton = () => {
+    //     setIsConnected(false);
+    //     sessionStorage.removeItem('customerSessionIdentifier');
+    //     socket.emit('customer:leave');
+    //     navigate('/');
+    // };
 
     useEffect(() => {
-        setFaqSection(sessionStorage.getItem('faqSection'));
-        setFaqQuestion(sessionStorage.getItem('faqQuestion'));
-
-        if (!(faqQuestion || faqQuestion)) {
+        if (!(sessionStorage.getItem('faqQuestion') || sessionStorage.getItem('faqSection'))) {
             // TODO: Handle nothing saved
         }
 
@@ -81,6 +81,22 @@ export default function InitialiseChat() {
         }
     }, []);
 
+    // Handle the display of the Suggest Appointment button
+    useEffect(() => {
+        // Increment inactivity time every minute
+        const interval = setInterval(() => {
+            setCustWaitingTime(prev => prev + 1);
+
+            if (custWaitingTime >= 5 || (waitingTime < 10 && custWaitingTime >= 3)) {
+                setDisplayReccoAppt(true);
+            }
+        }, 60000);
+
+        return () => {
+            clearInterval(interval);
+        }
+    }, []);
+
     useEffect(() => {
         if (isConnected) {
             setConnectionErr(false)
@@ -96,16 +112,30 @@ export default function InitialiseChat() {
               <h1 className="text-2xl font-semibold">OCBC Support | Live Chat</h1>
             </div>
             <div className="flex-grow overflow-hidden flex items-center justify-center">
-              <div className="flex flex-col items-center justify-center">
-                <p className="text-lg font-medium text-gray-800 mb-2">Please hold while we connect you to an agent.</p>
+              <div className="flex flex-col items-center justify-center p-10 bg-white drop-shadow-[0_0px_4px_rgba(0,0,0,.15)]">
+                <p className="text-xl font-medium text-gray-800 mb-2">Please hold while we connect you to an agent.</p>
                 <div className="w-8 h-8 border-4 border-t-4 border-gray-300 rounded-full animate-spin mb-2" style={{ borderTopColor: "#8b3d58" }}></div>
-                <p>Estimated Waiting Time: <span className="font-bold">{connectionErr ? "Loading queue information" : `${waitingTime} minutes`}</span></p>
+                <p>Estimated Waiting Time: <span className="font-bold">{connectionErr ? "Loading queue information..." : `${waitingTime} minutes`}</span></p>
                 <button
-                  className="mt-4 px-4 py-2 bg-ocbcred text-white rounded hover:bg-gray-300 focus:outline-none"
+                  className="mt-4 px-4 py-2 bg-ocbcred text-white rounded hover:bg-ocbcdarkred focus:outline-none"
                   onClick={handleDisconnection}
                 >
                   Cancel Chat
                 </button>
+
+                {/* Suggest Appointment */}
+                <div className={`border-t-2 border-neutral-200 py-2 mt-5 ${displayReccoAppt ? "block" : "hidden"}`}>
+                    <p className='font-semibold mb-3'>We're sorry for the wait. Consider making an appointment instead?</p>
+                    <button
+                        className='w-full py-2 px-4 flex flex-row rounded-xl bg-white drop-shadow-[0_0px_3px_rgba(0,0,0,.15)] items-center gap-3 text-ocbcred hover:text-ocbcdarkred'
+                        onClick={() => navigate('/appointments/branches')}
+                    >
+                        <div className='w-10 h-10 rounded-full bg-ocbcred/10 flex justify-center items-center'>
+                            <BsCalendarCheck className='text-xl' />
+                        </div>
+                        <p className='text-lg font-medium'>Make an Appointment</p>
+                    </button>
+                </div>
               </div>
             </div>
           </>
