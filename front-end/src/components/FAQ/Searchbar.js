e7c59d36a627fcd79397c80d6dd1c88ba5cbacd8
Hervin Sie <hervsie@gmail.com>
2024-11-14 19:39:29 +0800

    feat: added framer motion to search bar results

--- front-end/src/components/FAQ/Searchbar.js
+++ front-end/src/components/FAQ/Searchbar.js
@@ -1,8 +1,8 @@
 import { useState } from 'react';
 import { FaMagnifyingGlass } from "react-icons/fa6";
 import { liteClient as algoliasearch } from "algoliasearch/lite";
-import { InstantSearch, SearchBox, Hits, Configure, Highlight} from 'react-instantsearch';
-
+import { InstantSearch, SearchBox, Hits, Configure, Highlight } from 'react-instantsearch';
+import { motion } from 'framer-motion';
 
 const searchClient = algoliasearch("AFO67MRW1I", "9874cb69b99b02792082929c772d84d2");
 
@@ -28,43 +28,49 @@ export default function Searchbar() {
 
   return (
     <div className="relative mt-12 mb-12 max-w-4xl mx-auto">
-        <InstantSearch searchClient={searchClient} indexName="title">
-            <Configure hitsPerPage={5} />
+      <InstantSearch searchClient={searchClient} indexName="title">
+        <Configure hitsPerPage={5} />
 
-            <div className="relative">
-              <SearchBox
-                classNames={{
-                    root: "w-full",
-                    input: "w-full rounded-full bg-[#F9F9F9] text-[#454040] text-xl px-6 py-4 shadow-lg focus:outline-none focus:ring-2 focus:ring-[#D00E35]",
-                    submitIcon: "hidden",
-                    resetIcon: "hidden",
-                    loadingIcon: "hidden"
-                }}
-                placeholder="Example: I'm having problems signing in to my account"
-                onFocus={() => setIsFocused(true)}
-                onBlur={() => setIsFocused(false)}
-                //searchAsYouType={true} //uncomment when demoing
-              />
-                <span className="absolute right-4 top-2/4 transform -translate-y-2/4 text-[#454040]">
-                    <FaMagnifyingGlass className="w-6 h-6" />
-                </span>
-            </div>
+        <div className="relative">
+          <SearchBox
+            classNames={{
+              root: "w-full",
+              input: "w-full rounded-full bg-[#F9F9F9] text-[#454040] text-xl px-6 py-4 shadow-lg focus:outline-none focus:ring-2 focus:ring-[#D00E35]",
+              submitIcon: "hidden",
+              resetIcon: "hidden",
+              loadingIcon: "hidden"
+            }}
+            placeholder="Example: I'm having problems signing in to my account"
+            onFocus={() => setIsFocused(true)}
+            onBlur={() => setIsFocused(false)}
+            //searchAsYouType={true} //uncomment when demoing
+          />
+          <span className="absolute right-4 top-2/4 transform -translate-y-2/4 text-[#454040]">
+            <FaMagnifyingGlass className="w-6 h-6" />
+          </span>
+        </div>
 
-            {/* Display search suggestions when focused */}
-            {isFocused && (
-                <div className="absolute top-full left-0 w-full mt-2 p-3 bg-white z-10 shadow-lg rounded-lg text-black">
-                    <div className="flex flex-col gap-3 text-left">
-                        <Hits
-                            hitComponent={(props) => (
-                                <div onMouseDown={handleMouseDown} onMouseUp={handleMouseUp}>
-                                    <Hit {...props} />
-                                </div>
-                            )}
-                        />
+        {/* Display search suggestions when focused */}
+        <motion.div
+          initial={{ opacity: 0.2, y: -10 }}
+          animate={{ opacity: isFocused ? 1 : 0, y: isFocused ? 0 : -10 }}
+          transition={{ type: "spring", stiffness: 100, damping: 15 }}
+        >
+          {isFocused && (
+            <div className="absolute top-full left-0 w-full mt-2 p-3 bg-white z-10 shadow-lg rounded-lg text-black">
+              <div className="flex flex-col gap-3 text-left">
+                <Hits
+                  hitComponent={(props) => (
+                    <div onMouseDown={handleMouseDown} onMouseUp={handleMouseUp}>
+                      <Hit {...props} />
                     </div>
-                </div>
-            )}
-        </InstantSearch>
+                  )}
+                />
+              </div>
+            </div>
+          )}
+        </motion.div>
+      </InstantSearch>
     </div>
   );
 }
