06446bbe9315f9b8322a7496b6f2ca5f2f911961
Hervin Sie <hervsie@gmail.com>
2024-11-14 19:09:18 +0800

    fix: UI

--- front-end/src/components/FAQ/SectionContainer.js
+++ front-end/src/components/FAQ/SectionContainer.js
@@ -22,7 +22,7 @@ export default function SectionContainer({ icon, title, description }) {
             </div>
 
             <div className="mt-3 flex flex-row gap-2 items-center">
-                <a className="font-semibold text-ocbcred">Explore FAQ</a>
+                <a className="font-semibold text-ocbcred">Explore {title}</a>
                 <FaArrowRightLong className="fill-ocbcred font-light transform transition-transform duration-300 group-hover:translate-x-2" />
             </div>
         </div>
--- front-end/src/pages/Customer/CustomerLandingPage.js
+++ front-end/src/pages/Customer/CustomerLandingPage.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import NavBar from '../../components/Navbar';
+import Footer from '../../components/Footer';
 import LiveChatPopup from '../../components/Chat/LiveChatPopup';
 import { useState } from 'react';
-import { PiChats } from "react-icons/pi";
-import { BsCalendarCheck } from "react-icons/bs";
+import { PiChats, PiCalendarBlank } from "react-icons/pi";
 import { MdEditCalendar } from "react-icons/md";
 
 const CustomerLandingPage = () => {
@@ -19,87 +19,48 @@ const CustomerLandingPage = () => {
             </div>
 
             {/* Buttons */}
-            <div className="text-center mt-10">
-                <div className="flex justify-center items-center space-x-6 mt-4">
-                    <div className="w-[200px] h-[8px] bg-[#D00E35]" />
-                    <h2 className="text-[40px]">Quick Actions</h2>
-                    <div className="w-[200px] h-[8px] bg-[#D00E35]" />
-                </div>
-                {/* onClick={() => navigate("/appointments/viewBooking")} */}
+            <div className="bg-white font-inter">
+                <div className="text-center mt-10">
+                    <div className="flex justify-center items-center space-x-6 mt-4">
+                        <div className="w-[200px] h-[8px] bg-[#D00E35]" />
+                        <h2 className="text-[40px]">Quick Actions</h2>
+                        <div className="w-[200px] h-[8px] bg-[#D00E35]" />
+                    </div>
 
-                <div className="bg-white shadow-lg rounded-xl p-6 w-[400px] h-[250px] hover:shadow-2xl transition-shadow duration-300">
-                    <a href='/appointments/viewBooking'>
-                        <MdEditCalendar className="w-12 h-12 object-contain fill-ocbcred mb-4" />
-                        <h3 className="text-[24px] font-semibold text-left">View and Edit Appointments</h3>
-                        <p className="text-[18px] font-light text-left text-gray-500 mt-2">
-                            Re-schedule an appointment to an alternative timeslot
-                        </p>
-                    </a>
-                </div>
+                    <div className="flex flex-wrap justify-center space-x-8 mt-8 mx-12">
+                        <a className="bg-white shadow-lg rounded-xl p-6 w-[400px] h-[250px] hover:shadow-2xl transition-shadow duration-300 mb-8" href='/appointments/viewBooking'>
+                            <MdEditCalendar className="w-12 h-12 object-contain fill-ocbcred mb-4" />
+                            <h3 className="text-[24px] font-semibold text-left">View and Edit Appointments</h3>
+                            <p className="text-[18px] font-light text-left text-gray-500 mt-2">
+                                Re-schedule an appointment to an alternative timeslot
+                            </p>
+                        </a>
 
-                <div className="flex justify-between space-x-8 mt-8 mx-12">
-                    <a className="bg-white shadow-lg rounded-xl p-6 w-[400px] h-[250px] hover:shadow-2xl transition-shadow duration-300" href="/appointments/branches">
-                        <BsCalendarCheck className="w-12 h-12 object-contain fill-ocbcred mb-4" />
-                        <h3 className="text-[24px] font-semibold text-left">Book an Appointment</h3>
-                        <p className="text-[18px] font-light text-left text-gray-500 mt-2">
-                            Schedule an appointment for any queries you might have
-                        </p>
-                    </a>
-                </div>
+                        <a className="bg-white shadow-lg rounded-xl p-6 w-[400px] h-[250px] hover:shadow-2xl transition-shadow duration-300 mb-8" href="/appointments/branches">
+                            <PiCalendarBlank className="w-12 h-12 object-contain fill-ocbcred mb-4" />
+                            <h3 className="text-[24px] font-semibold text-left">Book an Appointment</h3>
+                            <p className="text-[18px] font-light text-left text-gray-500 mt-2">
+                                Schedule an appointment for any queries you might have
+                            </p>
+                        </a>
 
-                <div className="flex justify-between space-x-8 mt-8 mx-12">
-                    <button onClick={() => setIsOpen(true)} className="bg-white shadow-lg rounded-xl p-6 w-[400px] h-[250px] hover:shadow-2xl transition-shadow duration-300">
-                        <PiChats className="w-12 h-12 object-contain fill-ocbcred mb-4" />
-                        <h3 className="text-[24px] font-semibold text-left">Start a Live Chat</h3>
-                        <p className="text-[18px] font-light text-left text-gray-500 mt-2">
-                            Start a live chat session for any queries you might have
-                        </p>
-                    </button>
+                        <div onClick={() => setIsOpen(true)} className="bg-white shadow-lg rounded-xl p-6 w-[400px] h-[250px] hover:shadow-2xl transition-shadow duration-300 mb-8 cursor-pointer">
+                            <PiChats className="w-12 h-12 object-contain fill-ocbcred mb-4" />
+                            <h3 className="text-[24px] font-semibold text-left">Start a Live Chat</h3>
+                            <p className="text-[18px] font-light text-left text-gray-500 mt-2">
+                                Start a live chat session for any queries you might have
+                            </p>
+                        </div>
+                    </div>
+
+                    <hr className="border-t-[2px] border-[#DCD6D6] mt-16" />
                 </div>
-                <hr className="border-t-[2px] border-[#DCD6D6] mt-16" />
             </div>
 
             {isOpen && <LiveChatPopup isOpen={isOpen} setIsOpen={setIsOpen} />}
 
             {/* Footer */}
-            <footer className="py-12 bg-gray-50"> 
-                <div className="flex justify-between items-start px-8 lg:px-16">
-                    <div>
-                        <img src="/ocbc-logo.png" alt="OCBC Logo" className="w-[280px] h-[76px]" />
-                        <h3 className="text-[50px] mt-4">Group</h3>
-                    </div>
-                    <div className="flex space-x-12 ml-96"> 
-                        <div className="space-y-4">
-                            <h4 className="text-[20px]">Useful Links</h4>
-                            <p className="text-[16px]">Investor Information</p>
-                            <p className="text-[16px]">International Network</p>
-                            <p className="text-[16px]">Careers</p>
-                            <p className="text-[16px]">Research</p>
-                        </div>
-                        <div className="space-y-4">
-                            <h4 className="text-[20px]">Contact Us</h4>
-                            <p className="text-[16px]">Personal Banking</p>
-                            <p className="text-[16px]">Premier Banking</p>
-                            <p className="text-[16px]">FRANK by OCBC</p>
-                            <p className="text-[16px]">Business Banking</p>
-                        </div>
-                    </div>
-                    <div className="flex flex-col space-y-6">
-                        <div className="w-[50px] h-[50px] bg-[#2D3D45] rounded-full flex justify-center items-center hover:scale-105 transition-transform">
-                            <img src="/facebook.svg" alt="Facebook" className="w-2/5 h-2/5 object-contain" />
-                        </div>
-                        <div className="w-[50px] h-[50px] bg-[#2D3D45] rounded-full flex justify-center items-center hover:scale-105 transition-transform">
-                            <img src="/youtube.svg" alt="Youtube" className="w-2/5 h-2/5 object-contain" />
-                        </div>
-                        <div className="w-[50px] h-[50px] bg-[#2D3D45] rounded-full flex justify-center items-center hover:scale-105 transition-transform">
-                            <img src="/twitter.svg" alt="Twitter" className="w-2/5 h-2/5 object-contain" />
-                        </div>
-                        <div className="w-[50px] h-[50px] bg-[#2D3D45] rounded-full flex justify-center items-center hover:scale-105 transition-transform">
-                            <img src="/linkedin.svg" alt="Linkedin" className="w-2/5 h-2/5 object-contain" />
-                        </div>
-                    </div>
-                </div>
-            </footer>
+            <Footer />
         </div>
     );
 }
