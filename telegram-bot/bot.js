90958797c46f01d37bb1aa360d6c55afcfcff896
Ethan-Chew <ethanchewmh@gmail.com>
2025-01-12 20:07:00 +0800

    wip: trying to get the bot to work

---
+++ telegram-bot/bot.js
@@ -0,0 +1,39 @@
+import { Bot, GrammyError, HttpError } from "grammy";
+import { generateUpdateMiddleware } from "telegraf-middleware-console-time";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+// Controllers
+import { startController } from "./controllers/startController.js";
+import { linkController } from "./controllers/linkController.js";
+
+const token = process.env.TELEGRAM_API_KEY || "";
+const bot = new Bot(token);
+if (process.env.ENVIRONMENT === "development") {
+    bot.use(generateUpdateMiddleware());
+}
+
+// Command Handling
+bot.command("start", startController)
+bot.command("link", linkController);
+
+// Catch any errors
+bot.catch((err) => {
+    const ctx = err.ctx;
+    console.error(`Error while handling update ${ctx.update.update_id}:`);
+    const e = err.error;
+    if (e instanceof GrammyError) {
+      console.error("Error in request:", e.description);
+    } else if (e instanceof HttpError) {
+      console.error("Could not contact Telegram:", e);
+    } else {
+      console.error("Unknown error:", e);
+    }
+});
+
+// Start the Bot
+console.log("Telegram Bot Started");
+bot.start();
+
+export default bot;
--- telegram-bot/controllers/startController.js
+++ telegram-bot/controllers/startController.js
@@ -1,6 +1,8 @@
 import { Context } from "grammy";
+import axios from "axios";
 
-const startController = async (ctx: Context) => {
+const startController = async (ctx) => {
+    await ctx.reply("AWErgothf");
     await ctx.reply(`
         # OCBC Support Bot
         Hi, I am the 'official' Support Bot for OCBC, here to help you with your queries.
@@ -8,10 +10,14 @@ const startController = async (ctx: Context) => {
         - \`/start\` - Start the bot
         - \`/help\` - Get help
         - \`/link\` - Relink the bot to your OCBC Account
-    `, { parse_mode: "Markdown" });
+    `, { 
+        parse_mode: "Markdown",
+        reply_parameters: { message_id: ctx.msg.message_id },
+    });
+
     const payload = ctx.match;
     if (payload) {
-        ctx.chat
+        console.log(ctx.chat);
     }
 }
 
