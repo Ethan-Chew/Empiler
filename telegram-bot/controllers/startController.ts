0e88ae722012f3a3d5c790b3cac1bfd31167e1d1
Ethan Chew <ethanchewmh@gmail.com>
2025-01-30 01:33:54 +0800

    fix: update markdown version in start controller from V2 to v1

--- telegram-bot/controllers/startController.ts
+++ telegram-bot/controllers/startController.ts
@@ -4,7 +4,7 @@ import validateUUID from "../utils/validateUUID";
 
 const startController = async (ctx: Context) => {
     await ctx.reply(`__*OCBC Support Bot*__\nHi, I am the 'official' Support Bot for OCBC, here to help you with your queries.\n*Commands*\n- \`/start\` - Start the bot\n- \`/link\` - Link the bot to your OCBC Account.\n- \`/unlink\` - Unlink the bot from your OCBC Account \(you will not be able to access services until you re-link it.\)\n- \`/upcoming\` - View all Upcoming Appointments.`, { 
-        parse_mode: "MarkdownV2",
+        parse_mode: "Markdown",
         reply_parameters: { message_id: ctx.msg!.message_id },
     });
 
